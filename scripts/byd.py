6084989a826673a701cd74daa61f4e9b02437853
litequark <33547903+litequark@users.noreply.github.com>
2025-05-20 02:31:07 +0800

    redo logic according to ux

--- scripts/byd.py
+++ scripts/byd.py
@@ -49,41 +49,50 @@ with open(OUTPUT_PATH, "w", encoding="utf-8") as f: # 清除csv文件
     list_writer.writerow(RESULT_FIELDS)
 
 dealer_count = 0
-for dealer_type in {"0", "1"}:
+for sale_network in {"2", "3"}:
+    sale_network_literal = ""
+    if sale_network == "2":
+        sale_network_literal = "王朝"
+    elif sale_network == "3":
+        sale_network_literal = "海洋"
+    print(f"开始处理 {sale_network_literal}")
+
     city_ids = []
 
     # 获取省份列表
-    payload_provinces = {"dealerType": dealer_type}
+    payload_provinces = {"dealerType": "0"}
     api_province = API + "province"
     response_provinces = requests.post(url=api_province,
                                        data=json.dumps(payload_provinces, ensure_ascii=False),
                                        headers=DEFAULT_HEADERS).json()
 
-    province_count = len(response_provinces)
+    province_total = len(response_provinces["data"])
+    print(f"{sale_network_literal} 获取到 {province_total} 个省份信息")
 
     province_ids = []
     for m in response_provinces["data"]:
         province_ids.append(m["n_province_id"])
 
     # 分省处理
-    city_count = 0
+    city_total = 0
     api_city = API + "city"
     for province_id in province_ids:
         # 获取城市列表
-        payload_cities = {"dealerType": dealer_type, "provinceId": province_id}
+        payload_cities = {"dealerType": "0", "provinceId": province_id}
         response_cities = requests.post(url=api_city,
                                         data=json.dumps(payload_cities, ensure_ascii=False),
                                         headers=DEFAULT_HEADERS).json()
-        city_count += len(response_cities["data"])
+        city_total += len(response_cities["data"])
         for m in response_cities["data"]:
             city_ids.append(m["n_city_id"])
 
+    print(f"{sale_network_literal} 获取到 {city_total} 个城市信息")
     processed_city = 0
 
     # 分市处理
     api_dealer = API + "list"
     for city_id in city_ids:
-        payload_dealers = {
+        payload_dealers_default = {
             "dealerKey": "",
             "provinceId": "",
             "cityId": city_id,
@@ -93,84 +102,70 @@ for dealer_type in {"0", "1"}:
             "latitude": 22.316554,
             "pageNum": 0,
             "numPerPage": 1000,
-            "dealerType": dealer_type  # 0: 售前经销, 1: 售后服务
+            "dealerType": ""  # 0: 售前经销, 1: 售后服务
         }
 
-        # 查询王朝
-        header_dynasty = DEFAULT_HEADERS.copy()
-        header_dynasty.update({"salenetwork": "2"}) # 2：王朝，3：海洋
-        response_dealers_dynasty = requests.post(url=api_dealer,
-                                         data=json.dumps(payload_dealers, ensure_ascii=False),
-                                         headers=header_dynasty).json()
-        while response_dealers_dynasty["success"] is False:
-            print("HTTP请求成功但JSON返回失败，可能被限流，10秒后重试……\n")
-            sleep_with_random(10, 1)
-            response_dealers_dynasty = requests.post(url=api_dealer,
+        for dealer_type in {"0", "1"}:
+            header_dealers = DEFAULT_HEADERS.copy()
+            header_dealers.update({"salenetwork": sale_network})
+            payload_dealers = payload_dealers_default.copy()
+            payload_dealers.update({"dealerType": dealer_type})
+
+            response_dealers = requests.post(url=api_dealer,
                                              data=json.dumps(payload_dealers, ensure_ascii=False),
-                                             headers=header_dynasty).json()
-        sleep_with_random(1, 1)
-
-        # 查询海洋
-        header_ocean = DEFAULT_HEADERS.copy()
-        header_ocean.update({"salenetwork": "3"}) # 2：王朝，3：海洋
-        response_dealers_ocean = requests.post(url=api_dealer,
+                                             headers=header_dealers).json()
+            while response_dealers["success"] is False:
+                print("HTTP请求成功但JSON返回失败，可能被限流，10秒后重试……\n")
+                sleep_with_random(10, 1)
+                response_dealers = requests.post(url=api_dealer,
                                                  data=json.dumps(payload_dealers, ensure_ascii=False),
-                                                 headers=header_ocean).json()
-        while response_dealers_ocean["success"] is False:
-            print("HTTP请求成功但JSON返回失败，可能被限流，10秒后重试……\n")
-            sleep_with_random(10, 1)
-            response_dealers_ocean = requests.post(url=api_dealer,
-                                                     data=json.dumps(payload_dealers, ensure_ascii=False),
-                                                     headers=header_ocean).json()
-        sleep_with_random(1, 1)
+                                                 headers=header_dealers).json()
+            sleep_with_random(1, 1)
 
-        # 合并王朝和海洋
-        response_dealers_data = response_dealers_ocean["data"] + response_dealers_dynasty["data"]
+            for m in response_dealers["data"]:
+                dealer_count += 1
+                # 删除\n
+                for k in m: # keys
+                    if isinstance(m[k], str):
+                        m[k] = m[k].replace('\\n', ' ').replace('\n', ' ')
 
-        for m in response_dealers_data:
-            dealer_count += 1
-            # 删除\n
-            for k in m: # keys
-                if isinstance(m[k], str):
-                    m[k] = m[k].replace('\\n', ' ').replace('\n', ' ')
+                dealer_type_literal = ""
+                if dealer_type == "0":
+                    dealer_type_literal = "售前经销"
+                elif dealer_type == "1":
+                    dealer_type_literal = "售后服务"
 
-            dealer_type_literal = ""
-            if dealer_type == "0":
-                dealer_type_literal = "售前经销"
-            elif dealer_type == "1":
-                dealer_type_literal = "售后服务"
+                dealer_type2_literal = ""
+                has_attr = False
+                for attr in {"卫星", "城展", "服务", "4S"}:
+                    if attr in m["dealerName"] and '店' in m["dealerName"]:
+                        dealer_type2_literal += attr
+                        has_attr = True
+                if has_attr:
+                    dealer_type2_literal += '店'
 
-            dealer_type2_literal = ""
-            has_attr = False
-            for attr in {"卫星", "城展", "服务", "4S"}:
-                if attr in m["dealerName"] and '店' in m["dealerName"]:
-                    dealer_type2_literal += attr
-                    has_attr = True
-            if has_attr:
-                dealer_type2_literal += '店'
+                for attr in {"商超店", "城市展厅"}:
+                    if attr in m["dealerName"]:
+                        dealer_type2_literal += attr
 
-            for attr in {"商超店", "城市展厅"}:
-                if attr in m["dealerName"]:
-                    dealer_type2_literal += attr
-
-            dealer = {
-                    "省": m["provinceName"],
-                    "Province": "",
-                    "市": m["cityName"],
-                    "City": "",
-                    "区": "",
-                    "店名": m["dealerName"],
-                    "类型": dealer_type_literal,
-                    "类型2": dealer_type2_literal,
-                    "地址": m["dealerAddress"],
-                    "电话": m["dealerTel"],
-                    "备注": ""
-            }
-            print(dealer)
-            with open(OUTPUT_PATH, "a", encoding="utf-8", newline='') as f:
-                dict_writer = csv.DictWriter(f, fieldnames=RESULT_FIELDS, quoting=csv.QUOTE_ALL)
-                dict_writer.writerow(dealer)
+                dealer = {
+                        "省": m["provinceName"],
+                        "Province": "",
+                        "市": m["cityName"],
+                        "City": "",
+                        "区": "",
+                        "店名": m["dealerName"],
+                        "类型": dealer_type_literal,
+                        "类型2": dealer_type2_literal,
+                        "地址": m["dealerAddress"],
+                        "电话": m["dealerTel"],
+                        "备注": ""
+                }
+                print(dealer)
+                with open(OUTPUT_PATH, "a", encoding="utf-8", newline='') as f:
+                    dict_writer = csv.DictWriter(f, fieldnames=RESULT_FIELDS, quoting=csv.QUOTE_ALL)
+                    dict_writer.writerow(dealer)
         processed_city += 1
-        print("类型 " + dealer_type + ": 已处理" + str('%.2f' % ((processed_city / city_count) * 100)) + "%")
+        print(f"{sale_network_literal}: 已处理" + str('%.2f' % ((processed_city / city_total) * 100)) + "%")
 
 print("共计" + str(dealer_count) + "个门店")
