ba7ad23c755089adffedbed055d6b2e353ac32cd
litequark <33547903+litequark@users.noreply.github.com>
2025-05-25 13:52:11 +0800

    fix: add count

--- scripts/bridgestone.py
+++ scripts/bridgestone.py
@@ -228,92 +228,16 @@ try:
     '''获取城市首字母分组'''
     btn_alphabet_groups: list = btn_city_input_box.find_elements(by=By.CSS_SELECTOR, value='div.city_list ul.letter li')
 
+    store_total: int = 0
     '''依次处理所有的城市首字母分组'''
     for btn_alphabet_group in btn_alphabet_groups:
         wait.until(lambda _: btn_alphabet_group.is_displayed())
         # 跳过“热门”分组，只处理具体的首字母分组
         if btn_alphabet_group.text == '热门':
             continue
-        btn_alphabet_group.click()
+        store_total += process_btn_alphabet_group(btn_alphabet_group, btn_city_input_box)
 
-        # 获取当前城市首字母分组的container
-        container_alphabet_group = btn_city_input_box.find_element(by=By.CSS_SELECTOR,
-                                                                   value='div.city_list div.city_container.cc')
-        wait.until(lambda _: container_alphabet_group.is_displayed())
-
-        '''获取分组内所有行（一行城市为一个wrapper，内含城市和对应的区划列表，平级）'''
-        wrappers_cities: list = container_alphabet_group.find_elements(By.CSS_SELECTOR, 'ul.cities-wrapper')
-        for wrapper_cities in wrappers_cities:
-            wait.until(lambda _: wrapper_cities.is_displayed())
-            '''获取行内所有的城市按钮'''
-            btn_cities: list = wrapper_cities.find_elements(by=By.CSS_SELECTOR, value='li')
-
-            '''依次处理行内所有的城市'''
-            for btn_city in btn_cities:
-                wait.until(lambda _: btn_city.is_displayed())
-                str_city: str = btn_city.get_attribute("title")
-                btn_city.click()
-
-                # 全部区域 #city_span > div.city_list > div.city_container.cc > ul:nth-child(13) > div.district-content.cr > div:nth-child(1) > span
-                '''获取城市下辖的所有区划按钮'''
-                btn_districts: list = wrapper_cities.find_elements(by=By.CSS_SELECTOR,
-                                                                   value='div.district-content.cr div span')
-
-                '''依次处理城市下辖的所有区划'''
-                for btn_district in btn_districts:
-                    wait.until(lambda _: btn_district.is_displayed())
-                    if btn_district.text == '全部区域':
-                        continue
-                    str_district: str = btn_district.get_attribute("title")
-                    btn_district.click()
-                    '''获取“查询”按钮'''
-                    btn_search = btn_city_input_box.find_element(by=By.ID, value="search_btn_mylo2")
-                    wait.until(lambda _: btn_search.is_displayed())
-                    btn_search.click()
-
-                    '''等待查询完毕'''
-                    edit_field = driver.find_element(By.ID, value='edit-actions--2')
-                    wait.until(lambda _: edit_field.is_enabled())
-
-                    '''获取门店总数'''
-                    div_district_total_stores = driver.find_element(by=By.CSS_SELECTOR, value="#agency_length2")
-                    wait.until(lambda _: div_district_total_stores.is_displayed())
-                    str_district_total_stores: str = div_district_total_stores.text
-                    # 从字符串中提取数字
-                    match_store_count = re.search(r"(\d+)",
-                                                  str_district_total_stores) if str_district_total_stores else None
-                    int_district_total_stores: int = int(match_store_count.group()) if match_store_count else 0
-                    # 加入网页声称门店的计数
-                    claimed_store_count += int_district_total_stores
-                    print(int_district_total_stores)
-
-                    '''获取门店列表'''
-                    div_district_stores: list = driver.find_elements(By.CSS_SELECTOR, '#agencys-region2 div.store_list')
-                    # 加入实际爬取门店的计数
-                    scraped_store_count += len(div_district_stores)
-                    '''依次对列表里的店铺提取属性'''
-                    for div_district_store in div_district_stores:
-                        store_info: dict = STORE.copy()
-                        if div_district_store.is_displayed():
-                            parse_div_store_info(str_city, str_district, div_district_store, store_info)
-                        else:
-                            '''表示该元素处于下一页，需要翻页'''
-                            paginator_next = driver.find_element(By.CSS_SELECTOR,
-                                                                 '#agency_list2 div.page_bottom2 span.next2')
-                            wait.until(lambda _: paginator_next.is_displayed())
-                            paginator_next.click()
-                            wait.until(lambda _: div_district_store.is_displayed())
-                            parse_div_store_info(str_city, str_district, div_district_store, store_info)
-
-                        print(store_info)
-                        '''写入CSV'''
-                        save_store_info_to_csv(RESULT_FIELDS, store_info, OUTPUT_PATH)
-
-                    driver.find_element(by=By.ID, value='city_span').click()
-                    btn_alphabet_group.click()
-                    sleep(0.5)
-
-    print(f"共计{claimed_store_count}（声称的数量），{scraped_store_count}（实际爬取的）")
+    print(f"共计{store_total}家门店")
 
 except Exception as e:
     print(e)
