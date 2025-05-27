0d86f5f9536c6a580dab1cc4142244675bc03bea
南信李争之 <lilinhao5292006@163.com>
2025-05-27 13:14:00 +0800

    fixbug

--- scripts/huawei.py
+++ scripts/huawei.py
@@ -19,7 +19,7 @@ CSV_HEADER = ["品牌", "省", "Province", "市区辅助", "City/Area", "区",
               "店名", "类型", "地址", "电话", "备注"]
 API_URL = "https://cbg.huawei.com/isrp/lms/store-info/car-store-list/query"
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-OUTPUT_PATH = os.path.join(SCRIPT_DIR, "output/huaWei.csv")
+OUTPUT_PATH = os.path.join(SCRIPT_DIR, "output/huawei.csv")
 
 
 def fetch_stores(brand_code):
