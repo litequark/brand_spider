dca0063d093c218480438627818236b7634e866f
litequark <33547903+litequark@users.noreply.github.com>
2025-06-01 19:28:45 +0800

    headless

--- scripts/yokohoma.py
+++ scripts/yokohoma.py
@@ -102,7 +102,7 @@ def init_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument(f"user-agent={DEFAULT_UA}")
     # 设置默认拒绝所有网站的定位请求
